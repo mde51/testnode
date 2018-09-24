697a1893b81979851c3144be8a3213cf35d88378
Michel descotes <micheldescotes65@gmail.com>
2018-09-24 10:28:43 +0200

    testnode

---
+++ main.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk')
+console.log(chalk.red('Attention !'))
+console.log(chalk.green.underline('It\'s works !'))
+console.log(chalk.bgGreen.white.bold('Do NOT miss this'));
+console.log(chalk.blue.bgRed.bold('You are dead'));
